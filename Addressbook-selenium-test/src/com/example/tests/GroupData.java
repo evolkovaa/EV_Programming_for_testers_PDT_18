a1f99136bcfdbdd40d8f87ffd23c9fe178762850
evolkovaa <evolkova@naumen.ru>
2014-05-20 12:11:20 +0400

    Creation Groups Tests

---
+++ Addressbook-selenium-test/src/com/example/tests/GroupData.java
@@ -0,0 +1,18 @@
+package com.example.tests;
+
+public class GroupData {
+	public String name;
+	public String header;
+	public String footer;
+
+	public GroupData() {
+	}
+	
+	public GroupData(String groupname, String heater, String footer) {
+		this.name = groupname;
+		this.header = header;
+		this.footer = footer;
+	}
+	
+	
+}
