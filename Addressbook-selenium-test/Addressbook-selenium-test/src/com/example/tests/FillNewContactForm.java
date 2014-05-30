d54348297276d5f206a599aa8fc789d0606d1595
evolkovaa <evolkova@naumen.ru>
2014-05-30 15:27:38 +0400

    lesson 2 part1

---
+++ Addressbook-selenium-test/Addressbook-selenium-test/src/com/example/tests/FillNewContactForm.java
@@ -0,0 +1,9 @@
+package com.example.tests;
+
+public class FillNewContactForm {
+	public ContactDate contact;
+
+	public FillNewContactForm(ContactDate contact) {
+		this.contact = contact;
+	}
+}
---
+++ Addressbook-selenium-test/Addressbook-selenium-test/src/com/example/tests/GroupData.java
@@ -0,0 +1,18 @@
+package com.example.tests;
+
+public class GroupData {
+	public String name;
+	public String header;
+	public String footer;
+
+	public GroupData() {
+	}
+	
+	public GroupData(String name, String header, String footer) {
+		this.name = name;
+		this.header = header;
+		this.footer = footer;
+	}
+	
+	
+}
