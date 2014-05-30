18005c11fd6f4fb32bb1ccaa0826c11944fa2989
evolkovaa <evolkova@naumen.ru>
2014-05-30 18:48:29 +0400

    lesson 2 part 1

--- Addressbook-selenium-test/src/com/example/tests/ContactDate.java
+++ Addressbook-selenium-test/src/com/example/tests/ContactDate.java
@@ -32,24 +32,4 @@ public class ContactDate {
 		this.address2 = "";
 		this.home2 = "";
 	}
-
-	public ContactDate(String first, String last, String address,
-			String phoneHome, String phoneWork, String phoneMobile, String mail_1,
-			String mail_2, String bday, String bmonth, String year,
-			String GroupName, String address2, String home2) {
-		this.firstName = first;
-		this.lastName = last;
-		this.address = address;
-		this.phoneHome = phoneHome;
-		this.phoneMobile = phoneMobile;
-		this.phoneWork = phoneWork;
-		this.mail_1 = mail_1;
-		this.mail_2 = mail_2;
-		this.bday = bday;
-		this.bmonth = bmonth;
-		this.year = year;
-		this.groupName = groupName;
-		this.address2 = address2;
-		this.home2 = home2;
-	}
 }
--- Addressbook-selenium-test/src/com/example/tests/TestBase.java
+++ Addressbook-selenium-test/src/com/example/tests/TestBase.java
@@ -1,148 +1,21 @@
 package com.example.tests;
 
 
-import org.testng.Assert;
-import static org.testng.Assert.fail;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import com.example.fw.ApplicationManager;
 
 public class TestBase {
-
-	private static WebDriver driver;
-	private static String baseUrl;
-	private static boolean acceptNextAlert = true;
-	private static StringBuffer verificationErrors = new StringBuffer();
-
+	protected com.example.fw.ApplicationManager app;
 
 	@BeforeTest
 	public void setUp() throws Exception {
-	    driver = new FirefoxDriver();
-	    baseUrl = "http://localhost/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		app = new ApplicationManager();
 	  }
-	
 
 	@AfterTest
 	public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      Assert.fail(verificationErrorString);
-	    }
+		app.stop();
 	  }
-	
-
-	protected void returnToGroupsPage() {
-		driver.findElement(By.linkText("group page")).click();
-	  }
-
-	protected void submitGroupCreation() {
-		driver.findElement(By.name("submit")).click();
-	  }
-
-	protected void fillGroupForm(GroupData group) {
-		driver.findElement(By.name("group_name")).clear();
-		driver.findElement(By.name("group_name")).sendKeys(group.name);
-		driver.findElement(By.name("group_header")).clear();
-		driver.findElement(By.name("group_header")).sendKeys(group.header);
-		driver.findElement(By.name("group_footer")).clear();
-		driver.findElement(By.name("group_footer")).sendKeys(group.footer);
-	  }
-
-	protected void initGroupCreation() {
-		driver.findElement(By.name("new")).click();
-	  }
-
-	protected void gotoGroupsPage() {
-		driver.findElement(By.linkText("groups")).click();
-	  }
-
-	protected void openMainPage() {
-		driver.get(baseUrl + "/addressbookv4.1.4/");
-	  }
-
-	private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
-
-	private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-
-	protected void returnToHomePage() {
-	    driver.findElement(By.linkText("home page")).click();
-	}
-
-	protected void submitContactCreation() {
-	    driver.findElement(By.name("submit")).click();
-	}
-
-	protected void fillNewContactForm(ContactDate Contact) {
-	    driver.findElement(By.name("firstname")).clear();
-	    driver.findElement(By.name("firstname")).sendKeys(Contact.firstName);
-	    driver.findElement(By.name("lastname")).clear();
-	    driver.findElement(By.name("lastname")).sendKeys(Contact.lastName);
-	    driver.findElement(By.name("address")).clear();
-	    driver.findElement(By.name("address")).sendKeys(Contact.address);
-	    driver.findElement(By.name("home")).clear();
-	    driver.findElement(By.name("home")).sendKeys(Contact.phoneHome);
-	    driver.findElement(By.name("mobile")).clear();
-	    driver.findElement(By.name("mobile")).sendKeys(Contact.phoneMobile);
-	    driver.findElement(By.name("work")).clear();
-	    driver.findElement(By.name("work")).sendKeys(Contact.phoneWork);
-	    driver.findElement(By.name("email")).clear();
-	    driver.findElement(By.name("email")).sendKeys(Contact.mail_1);
-	    driver.findElement(By.name("email2")).clear();
-	    driver.findElement(By.name("email2")).sendKeys(Contact.mail_2);
-	    driver.findElement(By.name("address2")).clear();
-	    driver.findElement(By.name("address2")).sendKeys(Contact.address2);
-	    driver.findElement(By.name("phone2")).clear();
-	    driver.findElement(By.name("phone2")).sendKeys(Contact.home2);
-	}
-	
-	private void inputOfDate(String bday, String bmonth, String year, String groupName) {
-		new Select(driver.findElement(By.name("bday"))).selectByVisibleText(bday);
-		new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(bmonth);
-				   driver.findElement(By.name("byear")).clear();
-				   driver.findElement(By.name("byear")).sendKeys(year);
-		new Select(driver.findElement(By.name("GroupName"))).selectByVisibleText(groupName);
-					}
-
-	protected void gotoAddNewContactPage() {
-	    driver.findElement(By.linkText("add new")).click();
-	}
 
 }
